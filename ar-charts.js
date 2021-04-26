91644251b5950f9b92bf3822c3a71148da0c2389
LeeSquires <leesquires@me.com>
2021-04-26 10:39:25 +0100

    Create ar-charts.js

---
+++ ar-charts.js
@@ -0,0 +1,38 @@
+
+const data = {
+    labels : ["Maintenance-led Contracts","Management-led Contracts","Development"],
+    datasets: [
+        {
+        label: '2020',
+        data: [536.9, 258.5, 15.6],
+        backgroundColor: '#db042c',
+        borderColor: '#fff',
+        borderWidth: 2,
+        },
+        {
+        label: '2019',
+        data: [660.7, 184.0, 39.5],
+        borderColor: '#db042c',
+        borderWidth: 2,
+        backgroundColor: '#fff'
+        }
+    ]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        responsive: true,
+        indexAxis: 'y',
+    },
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: "Revenue",
+        },
+    }
+};
